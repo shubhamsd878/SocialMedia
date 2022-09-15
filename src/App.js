54c7866ce2fe8c56b88688884fb213d41afe3de8
vipul kumar <vipulkumar3254@gmail.com>
2022-09-15 14:26:34 +0530

    deleted some of waste files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
-import Footer from "./component/Footer";
-import Header from "./component/Header";
 import View from "./component/View";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Formfordev from "./component/Formfordev";
-import Upper from "./component/Upper";
 import './App.css'
 function App() {
   return (
@@ -12,10 +8,7 @@ function App() {
     <Routes>
       <Route path="/" element={ 
         <>
-          <Header name="Document" procs="Development" link="/dev" />
-          <Upper name="Document"/> 
           <View/>
-          <Footer/>
         </>}  
         />
     </Routes>
