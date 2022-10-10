42eb27320e831647b1e819c9cfc01125938a776e
vipul kumar <vipulkumar3254@gmail.com>
2022-10-10 09:57:53 +0530

    just don't know what is this

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import View from "./component/View";
+import Signup from "./component/authentication/Signup";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 function App() {
@@ -9,6 +10,11 @@ function App() {
       <Route path="/" element={ 
         <>
           <View/>
+        </>}  
+        />
+      <Route path="/signup" element={ 
+        <>
+       <Signup/>
         </>}  
         />
     </Routes>
