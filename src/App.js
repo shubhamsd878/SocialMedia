21af8602f35da358bf6fd4ae8dd6bbfdb5580271
Shubham <shubham.sd878@gmail.com>
2022-11-05 16:00:39 +0530

    vipul work redone --fetching post to frontend

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from "./component/Home";
 // import Header from './component/Navbar_Landing_Page'
 import Navbar_Landing_Page from "./component/Navbar_Landing_Page";
 import Navbar_Home_Page from "./component/Home/Navbar_Home_Page";
+import UserProfile from "./component/userProfile/UserProfile";
+
 function App() {
   return (
     <div className = "app">
@@ -25,7 +27,9 @@ function App() {
           </div>}  
         />)
       }
-
+      {/* // route for userProfile page */}
+      {/* <Route path='/userProfile' elemtent={<userProfile /> } /> */}
+      <Route path='/userProfile' element={<UserProfile /> } />
     </Routes>
   </BrowserRouter>
   </div>
---
+++ src/component/userProfile/UserProfile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './UserProfile.css'
+const UserProfile = () => {
+    
+    return(
+        <div className='userProfile'>
+            
+        </div>
+    )
+}
+export default UserProfile
